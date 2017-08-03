1a5f7381c1361f34e124f77173fe32bf6f3d42bc
Caleb.Chen <caleb.cheng@elitechtw01.com>
2017-08-03 13:01:48 +0800

    update

---
+++ spring-boot-cc/src/main/java/com/cc/MainConfig.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.cc;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Caleb Cheng
+ *
+ */
+@Configuration
+public class MainConfig {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
