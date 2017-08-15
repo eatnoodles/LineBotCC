f63a6110517b5a1fcbcfafdaa3b0663a3f38fd20
Caleb.Chen <caleb.cheng@elitechtw01.com>
2017-08-15 08:50:11 +0800

    fix

--- spring-boot-cc/src/main/java/com/cc/service/impl/IrolServiceImpl.java
+++ spring-boot-cc/src/main/java/com/cc/service/impl/IrolServiceImpl.java
@@ -106,7 +106,7 @@ public class IrolServiceImpl implements IIrolService {
 		
 		String text = NudoCCUtil.codeMessage("IRL006", monster.getHp(), monster.getAtk(), monster.getDef(), monster.getSpeed());
 		//FIXME hard code img
-		String img = "https://tw.meetgee.com/website_img/Files/Topic/20110817/fd1b64317b904fd4a0470c0c66b2a2ff.jpg";
+		String img = "https://tw.webimage.beanfun.com/uploadimg/LineageWEB/monster_image/lin20001130_47.jpg";
 		
 		ButtonsTemplate buttonsTemplate = new ButtonsTemplate(img, title, text, actions);
 		TemplateMessage result = new TemplateMessage(alt, buttonsTemplate);
