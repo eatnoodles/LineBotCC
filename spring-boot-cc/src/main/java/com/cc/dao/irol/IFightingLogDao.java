f64fed27cf81ca7ee1ec60d7af711827f4cc4977
Caleb.Chen <caleb.cheng@elitechtw01.com>
2017-08-16 14:53:57 +0800

    add default page

--- spring-boot-cc/src/main/java/com/cc/dao/irol/IFightingLogDao.java
+++ spring-boot-cc/src/main/java/com/cc/dao/irol/IFightingLogDao.java
@@ -1,7 +1,11 @@
 package com.cc.dao.irol;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -17,7 +21,10 @@ import com.cc.entity.irol.FightingLog;
 @Repository
 public interface IFightingLogDao extends CrudRepository<FightingLog, Long> {
 
-	@Query("select 1 from FightingLog a where a.lineId = :lineId and a.irol.id = :irolId and a.monster.id = :monsterId and a.status = 1 order by a.lastDttm desc")
-	public FightingLog findLastLog(@Param("lineId")String lineId, @Param("irolId")Long irolId, @Param("monsterId")Long monsterId);
+	@Query("select a from FightingLog a where a.lineId = :lineId and a.fightingIrolStatus.irol.id = :irolId and a.fightingMonsterStatus.monster.id = :monsterId and a.status = 1 order by a.lastDttm desc")
+	public List<FightingLog> findLog(@Param("lineId")String lineId, @Param("irolId")Long irolId, @Param("monsterId")Long monsterId, Pageable pageable);
 
+	default FightingLog findLastLog(String lineId, Long irolId, Long monsterId) {
+		return findLog(lineId, irolId, monsterId, new PageRequest(0, 1)).get(0);
+	}
 }
