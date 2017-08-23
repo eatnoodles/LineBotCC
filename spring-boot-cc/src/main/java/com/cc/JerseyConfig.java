51684dce95412da09ab49febff220eef51596c58
Caleb.Chen <caleb.cheng@elitechtw01.com>
2017-08-23 14:33:35 +0800

    img

---
+++ spring-boot-cc/src/main/java/com/cc/JerseyConfig.java
@@ -0,0 +1,22 @@
+package com.cc;
+
+
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+import com.cc.ws.NudoControl;
+
+
+/**
+ * @author Caleb Cheng
+ *
+ */
+@Configuration
+public class JerseyConfig extends ResourceConfig {
+
+	public JerseyConfig() {
+		register(NudoControl.class);
+	}
+
+}
