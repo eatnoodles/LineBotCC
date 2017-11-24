8489a0163d73feab3170d6d9f84e864d340dc105
Caleb.Chen <caleb.cheng@elitechtw01.com>
2017-11-24 13:56:42 +0800

    fix

--- spring-boot-cc/src/main/java/com/cc/service/impl/NudoCCServiceImpl.java
+++ spring-boot-cc/src/main/java/com/cc/service/impl/NudoCCServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.commons.lang3.StringUtils;
-import org.eclipse.jdt.internal.compiler.env.INameEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
