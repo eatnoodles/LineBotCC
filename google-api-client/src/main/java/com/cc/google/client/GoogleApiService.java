334a65b8a2463fc4c56994e7e75baefc065d4d16
Caleb.Chen <caleb.cheng@elitechtw01.com>
2017-12-07 14:13:43 +0800

    update to POST

--- google-api-client/src/main/java/com/cc/google/client/GoogleApiService.java
+++ google-api-client/src/main/java/com/cc/google/client/GoogleApiService.java
@@ -3,7 +3,7 @@ package com.cc.google.client;
 import com.cc.google.shortener.ShortenUrl;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Streaming;
 
 /**
@@ -18,6 +18,6 @@ public interface GoogleApiService {
      * @see <a href="https://developers.google.com/url-shortener/v1/getting_started">//https://developers.google.com/url-shortener/v1/getting_started</a>
      */
     @Streaming
-    @GET("url")
+    @POST("url")
     Call<ShortenUrl> getShortenURL(String url);
 }
