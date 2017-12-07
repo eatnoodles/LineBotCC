0fb952df7654dd10851a7f7f74c360ea6d674a6f
Caleb.Chen <caleb.cheng@elitechtw01.com>
2017-12-07 14:21:36 +0800

    no message

--- google-api-client/src/main/java/com/cc/google/client/GoogleApiService.java
+++ google-api-client/src/main/java/com/cc/google/client/GoogleApiService.java
@@ -3,8 +3,9 @@ package com.cc.google.client;
 import com.cc.google.shortener.ShortenUrl;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
-import retrofit2.http.Streaming;
 
 /**
  * 
@@ -17,7 +18,7 @@ public interface GoogleApiService {
      * 
      * @see <a href="https://developers.google.com/url-shortener/v1/getting_started">//https://developers.google.com/url-shortener/v1/getting_started</a>
      */
-    @Streaming
+	@FormUrlEncoded
     @POST("url")
-    Call<ShortenUrl> getShortenURL(String url);
+    Call<ShortenUrl> getShortenURL(@Field("longUrl")String url);
 }
